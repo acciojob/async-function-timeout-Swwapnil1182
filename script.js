d98f3fd4f7a06ca2270f8c2d7328bd4b0a49171a
Swwapnil1182 <swapniljadhav1182@gmail.com>
2025-05-23 18:22:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
-//your JS code here. If required.
 
-document.getElementById('btn').addEventListener('click',function(){
-    const outputDiv=document.getElementById('output')
+document.getElementById('btn').addEventListener('click', async function () {
+    const outputDiv = document.getElementById('output')
     const delay = document.getElementById('delay').value.trim()
+    const text = document.getElementById('text').value.trim()
 
 
-   outputDiv.textContent="";
-   if(isNaN(delay)){
+    outputDiv.textContent = "";
+    if (isNaN(delay)) {
         outputDiv.textContent = "Please enter a valid number.";
         return;
-   }
+    }
 
-   function promise1(text,delay) {
-    return new Promise((resolve) => {
-        
-        setTimeout(() => {
-            const text = document.getElementById('text').value
-            const delay = document.getElementById('delay').value.trim()
-            document.getElementById('output').textContent = `${text}`
-            resolve(text)
-        }, delay);
-    })
-}
- 
-promise1(text,delay)
+    function promise1(text, delay) {
+        return new Promise((resolve) => {
+
+            setTimeout(() => {
+
+                document.getElementById('output').textContent = `${text}`
+                resolve(text)
+            }, delay);
+        })
+    }
+    const result = await promise1(text, delay)
+
+    outputDiv.textContent = result
 })
 
 
 
 
 
-        
